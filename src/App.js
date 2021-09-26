e1bcafc1ebe8a096661b7d16bfe36588fac68fb0
Mrinal-KANTI55 <baust.cse.170101055@gmail.com>
2021-09-26 14:42:34 +0600

    work on header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='bodyOfHeader'>
+            <h1>Top Billionaire</h1>
+            <h4>make a conference for motivational speech for success in life</h4>
+        </div>
+    );
+};
+
+export default Header;
